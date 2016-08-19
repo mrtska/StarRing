d8765ca171e6dbab09c6bb319e397a3f759abe06
mrtska <mrtska.starring@gmail.com>
2016-08-19 13:53:42 +0900

    Add comment.

--- StarRing/include/list.h
+++ StarRing/include/list.h
@@ -46,7 +46,6 @@ public:
 		this->prev->next = this->next;
 		this->next->prev = this->prev;
 
-
 		//初期化
 		this->prev = this->next = this;
 	}
@@ -59,25 +58,28 @@ public:
 	}
 
 
-
 	//リストが空だったらnextやprevに自分のポインタが入っているはず
 	bool is_empty() {
 
 		return this->next == this;
 	}
 
-
+	//リストの先頭の値を返す 空のリストにやると面倒なことになる
 	T *first(unsigned long offset) {
 
 		return container_of<T>(this->next, reinterpret_cast<unsigned char*>(offset));
 	}
 
+	//リストの中身を返す リストの先頭で呼び出すと面倒なことになる
 	T *value(unsigned long offset) {
 
 		return container_of<T>(this, reinterpret_cast<unsigned char*>(offset));
 	}
 
 
+	//リストをfor eachする
+	//空のリストでは何も起きない
+	//for each中にデータの追加/削除が伴う場合は下のforeach_safeを使う 使わないとpos != this;が一生trueになる
 	template <typename Func>
 	void foreach(unsigned long offset, Func func) {
 
@@ -89,11 +91,11 @@ public:
 		}
 	}
 
+	//for each中にリストの変更が出来る
 	template <typename Func>
 	void foreach_safe(unsigned long offset, Func func) {
 
 		class list<T> *pos;
-
 		class list<T> *tmp;
 
 		for(pos = this->next, tmp = pos->next; pos != this; pos = tmp, tmp = pos->next) {
@@ -103,6 +105,8 @@ public:
 		}
 	}
 
+	//リストをリセットする
+	//コンストラクタが呼ばれない特殊な状態のときに使う
 	void reset() {
 
 		this->prev = this;
@@ -113,35 +117,7 @@ public:
 
 		reset();
 	}
-
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
